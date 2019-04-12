a85d778af8dca8535ba71e4f8ea1ba27a3ed99f4
Baggio Shehadi <k3nh1n@yahoo.com>
2019-04-12 12:58:55 -0500

    adding placeholder data from a fake server

--- src/App.vue
+++ src/App.vue
@@ -20,11 +20,7 @@ export default {
   },
   data() {
     return {
-      todos: [
-        { id: 1, title: "todo one", completed: false },
-        { id: 2, title: "todo two", completed: false },
-        { id: 3, title: "todo three", completed: true }
-      ]
+      todos: []
     };
   },
   methods: {
