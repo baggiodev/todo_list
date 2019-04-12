1237a72460865bb4ccb58d9f599e3c492487b8fd
Baggio Shehadi <k3nh1n@yahoo.com>
2019-04-12 13:30:03 -0500

    fixed adding w/ placeholder server

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
 import Todos from "./components/Todos";
 import Header from "./components/layout/Header";
 import AddTodo from "./components/AddTodo";
+import axios from "axios";
 
 export default {
   name: "app",
@@ -28,8 +29,21 @@ export default {
       this.todos = this.todos.filter(todo => todo.id !== id);
     },
     pushTodo(newTodo) {
-      this.todos = [...this.todos, newTodo];
+      const { title, completed } = newTodo;
+      axios
+        .post("https://jsonplaceholder.typicode.com/todos", {
+          title,
+          completed
+        })
+        .then(res => (this.todos = [...this.todos, res.data]))
+        .catch(err => console.log(err));
     }
+  },
+  created() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then(res => (this.todos = res.data))
+      .catch(err => console.log(err));
   }
 };
 </script>
